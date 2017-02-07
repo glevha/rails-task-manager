c4bc1486b0fb1f99fcdc39e5228c3ee46d14cd92
Gauthier Levha <levha.g@gmail.com>
2017-02-07 13:34:16 +0100

    gem added + controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170207122043) do
-
-  create_table "tasks", force: :cascade do |t|
-    t.string   "name"
-    t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.boolean  "done"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Task.create(name: "courses", description: "acheter pates, viande, vin", done: false)
-Task.create(name: "lettre", description: "envoyer lettre", done: false)
-Task.create(name: "loyer", description: "payer loyer", done: false)
+Task.create(name: "courses", description: "acheter pates, viande, vin")
+Task.create(name: "lettre", description: "envoyer lettre")
+Task.create(name: "loyer", description: "payer loyer")
